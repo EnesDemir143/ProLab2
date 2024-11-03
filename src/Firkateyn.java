8f56ab51dcf308029bd3470bddfdfcef39ab6293
EnesDemir143 <enesd41@outlook.com>
2024-11-03 21:28:24 +0300

    Deniz araçları constructor tamam

--- src/Firkateyn.java
+++ src/Firkateyn.java
@@ -1,7 +1,7 @@
 public class Firkateyn extends Deniz_araclari{
 
-    public Firkateyn(int seviye_puani, int dayaniklilik, int vurus, String sinif) {
-        super(seviye_puani, dayaniklilik, vurus, sinif);
+    public Firkateyn() {
+        super(0, 25, 10);
     }
 
 }
--- src/Sida.java
+++ src/Sida.java
@@ -1,6 +1,6 @@
 public class Sida extends Deniz_araclari{
 
-    public Sida(int seviye_puani, int dayaniklilik, int vurus, String sinif) {
-        super(seviye_puani, dayaniklilik, vurus, sinif);
+    public Sida() {
+        super(0, 15, 10);
     }
 }
