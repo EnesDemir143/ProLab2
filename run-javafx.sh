afa67241314ac274f3ee706716fc4b71c29efb32
JiEunA1 <149578916+EnesDemir143@users.noreply.github.com>
2024-11-29 19:03:15 +0300

    Update run-javafx.sh performance issues fixed

--- run-javafx.sh
+++ run-javafx.sh
@@ -6,8 +6,5 @@ JAVA_FX_PATH="/Library/Java/javafx-sdk-17.0.13/lib"
 # Çalıştırılacak JAR dosyasının yolu
 JAR_FILE="/Users/enesdemir/Desktop/ProLab2.jar"
 
-# Grafik işlemeyi yazılımsal olarak zorlamak için bir seçenek (isteğe bağlı)
-PRISM_OPTION="-Dprism.order=sw"
-
 # Java komutu ile uygulamayı çalıştırın
-java $PRISM_OPTION --module-path $JAVA_FX_PATH --add-modules javafx.controls,javafx.fxml -jar $JAR_FILE
+java --module-path $JAVA_FX_PATH --add-modules javafx.controls,javafx.fxml -jar $JAR_FILE
